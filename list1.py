60e300d3884d22ccc8b815ef12df3ab219cb7923
Kosmo <aryan.astro27@gmail.com>
2026-02-14 16:22:25 +0100

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,18 @@
+list = ['Apple', 'Mango', 'Orange', 'Lychee', 'Pineapple']
+print(list)
+print(len(list))
+print(list[-1])
+print(list[1:2+1])
+list.append ("Aryan")
+print(list)
+list.remove("Aryan")
+print(list)
+list.pop(4)
+print(list)
+list2 = [1, 27, 7, 3, 25, 13, 15]
+list2.sort()
+print(list2)
+list2.reverse()
+print(list2)
+list2.clear
+print(list2)
